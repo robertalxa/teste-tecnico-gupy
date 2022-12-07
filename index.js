cba8500108c3490fafa016d38d06f32a072e6d92
Robert <robert.alexandrea7@gmail.com>
2022-12-06 23:41:50 -0300

    Finalizado exercício 3

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import Exercicio1 from "./models/Exercicio1.js";
 import Exercicio2 from "./models/Exercicio2.js";
+import dados from "./data/dados.json" assert {type: 'json'};
+import Exercicio3 from "./models/Exercicio3.js";
 
 const ex1 = new Exercicio1();
 console.log(ex1.SOMA);
 
 const ex2 = new Exercicio2(21);
 console.log(ex2.buscaFibonacci());
+
+const ex3 = new Exercicio3(dados);
+console.log(`Os dias com menores valores de faturamento foram os dias ${ex3.buscaCondicionada('MENOR').map(dia => dia.dia)}`);
+console.log(`Os dias com maiores valores de faturamento foram os dias ${ex3.buscaCondicionada('MAIOR').map(dia => dia.dia)}`);
+console.log(`Número de dias no mês em que o valor de faturamento diário foi superior à média mensal ${ex3.diasSuperioresMedia()}`);
---
+++ models/Exercicio3.js
@@ -0,0 +1,44 @@
+export default class Exercicio3 {
+    _dados = [];
+
+    constructor (dados) {
+        this._dados = dados;
+    }
+
+    buscaCondicionada(condicao = '') {
+        if (!condicao) {
+            console.error('Escolha uma condição para a busca, "MAIOR" ou "MENOR"');
+            return [];
+        }
+
+        let menoresDias = [];
+        let menorDia = this._dados[0] ? this._dados[0] : undefined;
+
+        for (let dia of this._dados) {
+            if (condicao === 'MAIOR') {
+                if (dia.valor > menorDia.valor) menorDia = dia;
+            } else if (condicao === 'MENOR') {
+                if (dia.valor < menorDia.valor) menorDia = dia;
+            } else {
+                console.error('Escolha uma condição para a busca, "MAIOR" ou "MENOR"');
+                return [];
+            }
+        }
+
+        //Buscando dias iguais
+        for (let dia of this._dados) if (dia.valor === menorDia.valor) menoresDias.push(dia);
+
+        return menoresDias;
+    }
+
+    diasSuperioresMedia() {
+        let soma = 0;
+        for (let dia of this._dados) soma += dia.valor;
+        
+        const media = soma / this._dados.length;        
+        let diasSuperiores = this._dados.filter(dia => dia.valor > media);
+
+        return diasSuperiores.length;
+    }
+
+}
